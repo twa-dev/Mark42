507121e7bde17b66f5b21cbd204433ed70131caf
Artur Stambultsian <bumbledos@gmail.com>
2022-08-19 19:11:14 +0400

    storybook: removed decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import { AppearanceProvider } from "../src";
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -10,29 +7,3 @@ export const parameters = {
     },
   },
 };
-
-export const decorators = [
-  (Story) => {
-    const [theme, setTheme] = useState("apple");
-
-    return (
-      <div>
-        <div style={{ marginBottom: 10, fontFamily: "sans-serif" }}>
-          <label>
-            Theme:{" "}
-            <select
-              value={theme}
-              onChange={(e) => setTheme(e.currentTarget.value)}
-            >
-              <option value="material">Material</option>
-              <option value="apple">Apple</option>
-            </select>
-          </label>
-        </div>
-        <AppearanceProvider theme={theme}>
-          <Story />
-        </AppearanceProvider>
-      </div>
-    );
-  },
-];
