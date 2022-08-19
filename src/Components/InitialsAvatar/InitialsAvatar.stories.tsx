a97a0edbc127e2bded58acda65b6a278e81c5c60
Artur Stambultsian <bumbledos@gmail.com>
2022-08-19 19:10:10 +0400

    InitialsAvatar.stories.tsx: supported theme control

--- src/Components/InitialsAvatar/InitialsAvatar.stories.tsx
+++ src/Components/InitialsAvatar/InitialsAvatar.stories.tsx
@@ -10,12 +10,16 @@ export const InitialsAvatar: ComponentStory<typeof Component> = ({
   userId,
   userName,
   size,
+  theme,
 }) => {
-  return <Component size={size} userId={userId} userName={userName} />;
+  return (
+    <Component size={size} userId={userId} userName={userName} theme={theme} />
+  );
 };
 
 InitialsAvatar.args = {
   userName: "Artur Stambultsian",
   userId: 1,
   size: 40,
+  theme: "apple",
 };
